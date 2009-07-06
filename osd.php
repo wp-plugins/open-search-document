03cc8322749461003da6fa5d5eb3699136cf7ab7
pfefferle <pfefferle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-06 22:07:29 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/open-search-document/trunk@132538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- osd.php
+++ osd.php
@@ -1,14 +1,14 @@
 <?php 
 /*
-Plugin Name: OSD Maker
-Plugin URI: http://www.john-noone.com
+Plugin Name: Open Search Document Maker
+Plugin URI: http://wordpress.org/extend/plugins/open-search-document/
 Description: Create an Open Search Document for your blog.
-Version: 0.1
-Author: XBA
-Author URI: http://www.john-noone.com
+Version: 1.0
+Author: XBA, Matthias Pfefferle
+Author URI: http://wordpress.org/extend/plugins/open-search-document/
 */
 
-/*  Copyright 2006  XBA  (email : clint_northwood@the-fastest.net)
+/*  Copyright 2006  XBA, Matthias Pfefferle
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,56 +25,81 @@ Author URI: http://www.john-noone.com
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-$xba_osd = new xba_osd();
+// register
+if (isset($wp_version)) {
+	add_filter('query_vars', array('OpenSearchDocument', 'add_query_vars'));
+	add_filter('generate_rewrite_rules', array('OpenSearchDocument', 'rewrite_add_rule'));
+	add_action('parse_query', array('OpenSearchDocument', 'execute_request'));
+	add_action('wp_head', array('OpenSearchDocument', 'display_in_header'));
+	add_filter('get_profile_uri', array('OpenSearchDocument', 'head_profile'));
+	add_filter('xrds_simple', array('OpenSearchDocument', 'xrds_simple'));
+}
 
-class xba_osd {
-	function xba_osd() {
-		add_filter('query_vars', array(&$this, 'add_query_vars'));
-		add_filter('rewrite_rules_array', array(&$this, 'rewrite_add_rule'));
-		add_action('template_redirect', array(&$this, 'execute_request'));
-		add_action('wp_head', array(&$this, 'display_in_header'));
-		add_filter('head_profile', array(&$this, 'head_profile'));
-	}
-
+class OpenSearchDocument {
 	function add_query_vars( $vars ) {
-		$vars[] = 'request_osd';
+		$vars[] = 'opensearch';
 		return $vars;
 	}
 	
-	function rewrite_add_rule( $rules ) {
+	function rewrite_add_rule( $wp_rewrite ) {
 		global $wp_rewrite;
-		$url = get_bloginfo('url');
-		$rules['osd[.]xml$'] = 'index.php?request_osd=true';
-		return $rules;
+		$new_rules = array(
+			'osd.xml'		 => 'index.php?opensearch=true',
+			'opensearch.xml' => 'index.php?opensearch=true',
+      		'opensearch' 	 => 'index.php?opensearch=true',
+    	);
+    	$wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
 	}
+	
+	/**
+	 * Get the URL for the open-search-document, based on the blog's permalink settings.
+	 *
+	 * @return string open-search-document URL
+	 */
+	function get_url() {
+		global $wp_rewrite;
+	
+	    $url = trailingslashit(get_option('home'));
+		if($_SERVER['HTTPS'])
+	    	$url = preg_replace('/^http:/', 'https:', $url);
+	
+	    if ($wp_rewrite->using_permalinks()) {
+	    	if ($wp_rewrite->using_index_permalinks()) {
+	        	return $url . 'index.php/opensearch';
+	        } else {
+	        	return $url . 'opensearch';
+	        }
+	    } else {
+	    	return add_query_arg('opensearch', true, $url);
+	    }
+	}
+	
 	
 	function execute_request() {
 		global $wp;
 		#$wp->parse_request();
-		if( $wp->query_vars['request_osd'] ) { 
-			header('Content-Type: text/xml');
+		if( $wp->query_vars['opensearch'] ) { 
+			header('Content-Type: application/opensearchdescription+xml');
 			header('Encoding:utf-8');
 			echo '<?xml version="1.0" encoding="UTF-8"?>';
 	?>
-	<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearchdescription/1.1/">
+	<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
 	<ShortName><?php bloginfo('name'); ?></ShortName>
-	<Description>Chercher des informations sur le site <?php bloginfo('name'); ?></Description>
+	<Description><?php bloginfo('description'); ?></Description>
 	<Url type="text/html" method="get" template="<?php bloginfo('url'); ?>/?s={searchTerms}"></Url>
+	<Url type="application/atom+xml" template="<?php bloginfo('atom_url'); ?>?s={searchTerms}"/>
+   	<Url type="application/rss+xml" template="<?php bloginfo('rss2_url'); ?>?s={searchTerms}"/>
 	<Contact><?php bloginfo('admin_email'); ?></Contact>
-	<LongName>Chercher sur <?php bloginfo('name'); ?></LongName>
+	<LongName>Search through <?php bloginfo('name'); ?></LongName>
 	<Tags>wordpress blog</Tags>
-	<Image height="32" width="32" type="image/png"><?php bloginfo('url'); ?>/favicon.png</Image>
-	<Image height="16" width="16" type="image/png"><?php bloginfo('url'); ?>/favicon.png</Image>
-	<Image height="32" width="32" type="image/vnd.microsoft.icon"><?php bloginfo('url'); ?>/favicon.ico</Image>
+	<Image height="16" width="16" type="image/x-icon"><?php bloginfo('url'); ?>/favicon.ico</Image>
 	<Image height="16" width="16" type="image/vnd.microsoft.icon"><?php bloginfo('url'); ?>/favicon.ico</Image>
 	<Query role="example" searchTerms="blog"/>
-	<Developer>XBA</Developer>
+	<Developer>XSD, Matthias Pfefferle</Developer>
 	<Attribution>
-	    Search data copyright 2007, <?php bloginfo('name'); ?>, Some Rights Reserved. CC by-nc 2.5.
+	    Search data copyright 2009, <?php bloginfo('name'); ?>, Some Rights Reserved. CC by-nc 2.5.
 	</Attribution>
-	<SyndicationRight>open</SyndicationRight>
-	<AdultContent>false</AdultContent>
-	<Language>fr</Language>
+	<Language><?php bloginfo('language'); ?></Language>
 	<OutputEncoding>UTF-8</OutputEncoding>
 	<InputEncoding><?php bloginfo('charset'); ?></InputEncoding>
 	</OpenSearchDescription><?php
@@ -83,19 +108,30 @@ class xba_osd {
 		else return;
 	}
 	function display_in_header() {
-		echo '<link rel="search" type="application/opensearchdescription+xml" title="'. get_bloginfo('name') .'" href="'. get_bloginfo('url') .'/osd.xml" />'."\n";
+		echo '<link rel="search" type="application/opensearchdescription+xml" title="'. get_bloginfo('name') .'" href="'.OpenSearchDocument::get_url().'" />'."\n";
 	}
+
 	function head_profile($profiles) {
 		$profiles[] = 'http://a9.com/-/spec/opensearch/1.1/';
 		return $profiles;
 	}
-}
-
-if(!function_exists('head_profile')) {
-	function head_profile() {
-		$profiles[] = 'http://gmpg.org/xfn/11';
-		$profiles = apply_filters('head_profile', $profiles);
-		echo implode(" ", $profiles);
-	}
+	
+	/**
+	 * Contribute the Search to XRDS-Simple.
+	 *
+	 * @param array $xrds current XRDS-Simple array
+	 * @return array updated XRDS-Simple array
+	 */
+	function xrds_simple($xrds) {
+		$xrds = xrds_add_service($xrds, 'main', 'OpenSearchDocument', 
+	    	array(
+	        	'Type' => array( array('content' => 'http://a9.com/-/spec/opensearch/1.1/') ),
+	            'MediaType' => array( array('content' => 'application/opensearchdescription+xml') ),
+	            'URI' => array( array('content' => OpenSearchDocument::get_url()) )
+	        )
+	    );
+	
+	    return $xrds;
+	}	
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -1,16 +1,16 @@
 === Open Search Document ===
-Contributors: johnnoone
-Donate link: http://john-noone.com/
+Contributors: Matthias Pfeffele, johnnoone
+Donate link:
 Tags: open search document, osd, search, browser, interface
 Requires at least: 2.1
-Tested up to: 2.2
-Stable tag: trunk
+Tested up to: 2.8
+Stable tag: 1.0
 
 Create an Open Search Document for your blog.
 
 == Installation ==
 
-1. Upload `osd.php` to the `/wp-content/plugins/` directory
+1. Upload `open-search-document`-folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the *Plugins* menu in WordPress
 3. Replace your `<header>` tags by `<head profile="<?php head_profile(); ?>` in your templates
 4. Reload the permalinks trough the *Options > Permalinks* menu
